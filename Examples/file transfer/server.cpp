688540cff99f8526563627ff0933e5201edfcf66
glop102 <alexlikeschess@gmail.com>
2015-10-07 10:52:48 -0400

    Prep for threading

--- Examples/file transfer/server.cpp
+++ Examples/file transfer/server.cpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 void error(const char *msg)
 {
@@ -13,31 +14,8 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char** args){
-	int port=63036;
-	socklen_t clientLength;
-	struct sockaddr_in serv_addr, cli_addr;
-	int listenFD;
-	int newFD;
-
-	listenFD = socket(AF_INET, SOCK_STREAM, 0); //voodoo line 1
-	if (listenFD < 0) 
-        error("ERROR opening socket");
-    serv_addr.sin_family = AF_INET; //internet domain, not unix domain
-	serv_addr.sin_addr.s_addr = INADDR_ANY; //allow us to listen to all interfaces
-	serv_addr.sin_port = htons(port); //change order of the bytes from the host specific to the network order (so dont worry about Big or Little endian)
-	if (bind(listenFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-		error("ERROR on binding");
-
-	listen(listenFD,5); //start listening so that we can get new connections
-	clientLength = sizeof(cli_addr); //size of the struct
-	char buffer[256];
-
-	while(1){
-		int newSockFD = accept(listenFD, (struct sockaddr *) &cli_addr, &clientLength); //get the FD of the new client connection
-		if (newSockFD < 0) 
-	          error("ERROR on accept");
-	    printf("\n\n=== New Connection ===\n\n");
+void downloadFile(int newSockFD,struct sockaddr_in cli_addr){
+	printf("\n\n=== New Connection ===\n\n");
 	    char clientAddress[INET6_ADDRSTRLEN+1]; bzero(clientAddress,sizeof(clientAddress));
 	    inet_ntop(AF_INET, &(cli_addr.sin_addr),clientAddress,sizeof(clientAddress));
 	    printf("clientAddress\t%s\n",clientAddress);
@@ -56,7 +34,7 @@ int main(int argc, char** args){
 	    		filenameLength+=bbb-'0';
 	    	}else break;
 	    }
-	    if(filenameLength==0){close(newSockFD);continue;}
+	    if(filenameLength==0){close(newSockFD);return;}
 	    printf("Filename Length: %d\n", filenameLength);
 	    char filename[filenameLength+1];
 	    bzero(filename,filenameLength+1);
@@ -72,7 +50,7 @@ int main(int argc, char** args){
 	    	}
 	    	curt++;
 	    }
-	    if(filename[0]==0){close(newSockFD);continue;}
+	    if(filename[0]==0){close(newSockFD);return;}
 	    printf("%s\n", filename);
 	    read(newSockFD,&bbb,1); //clear out the newline seperator
 
@@ -89,7 +67,7 @@ int main(int argc, char** args){
 	    		fileLength+=bbb-'0';
 	    	}else break;
 	    }
-	    if(fileLength==0){close(newSockFD);continue;}
+	    if(fileLength==0){close(newSockFD);return;}
 	    printf("File     Length: %d\n", fileLength);
 
 	    //time to read the file and write to a file
@@ -107,6 +85,32 @@ int main(int argc, char** args){
 	    printf("Done Reading File\n");
 
 	    close(newSockFD);
+}
+
+int main(int argc, char** args){
+	int port=63036;
+	socklen_t clientLength;
+	struct sockaddr_in serv_addr, cli_addr;
+	int listenFD;
+
+	listenFD = socket(AF_INET, SOCK_STREAM, 0); //voodoo line 1
+	if (listenFD < 0) 
+        error("ERROR opening socket");
+    serv_addr.sin_family = AF_INET; //internet domain, not unix domain
+	serv_addr.sin_addr.s_addr = INADDR_ANY; //allow us to listen to all interfaces
+	serv_addr.sin_port = htons(port); //change order of the bytes from the host specific to the network order (so dont worry about Big or Little endian)
+	if (bind(listenFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+		error("ERROR on binding");
+
+	listen(listenFD,5); //start listening so that we can get new connections
+	clientLength = sizeof(cli_addr); //size of the struct
+	char buffer[256];
+
+	while(1){
+		int newSockFD = accept(listenFD, (struct sockaddr *) &cli_addr, &clientLength); //get the FD of the new client connection
+		if (newSockFD < 0) 
+	          error("ERROR on accept");
+	    downloadFile(newSockFD,cli_addr);
 	}
 
 	printf("Exiting\n");
