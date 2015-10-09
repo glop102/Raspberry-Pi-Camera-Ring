da45a6e0cf6f028e493776771ef7f0b78c79d8b0
glop102 <alexlikeschess@gmail.com>
2015-10-08 20:44:21 -0400

    Made accepting new connections multithreaded

--- Admin/main.cpp
+++ Admin/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "../libs/stdSocketTools.h"
 #include "webServer.h"
 #include "raspiControlSystem.h"
@@ -15,6 +16,38 @@ void error(const char *msg)
     exit(1);
 }
 
+void* spawnThread(void* peer){
+	int newSockFD=((struct newConnectionInfo*)peer)->FD;
+
+	char buf;
+	int charsRead=1;
+    std::string header;
+    while(charsRead){
+	    charsRead=read(newSockFD,&buf,1);//read the message
+	    if(charsRead<0) error("Unable to read socket");
+	    printf("%c", buf);
+	    header+=buf;
+
+	    int headerLength=header.length();
+	    if(headerLength<5) continue;
+	    if(header.substr(headerLength-4) == "\r\n\r\n"){
+	    	//found the end of the header of the newly connected client
+	    	//now lets break so that we can analyze what we need to do
+	    	break;
+	    }
+	}
+	printf("\n\n=== Writing ===\n\n");
+
+	if(header.substr(0,3)=="GET")
+		engageBrowser(newSockFD,"");
+	if(header.substr(0,4)=="POST")
+		engageBrowser(newSockFD,"");
+	if(header.substr(0,5)=="RASPI")
+		raspiConnection(*(struct newConnectionInfo*)peer,header);
+
+	delete (struct newConnectionInfo*)peer;
+}
+
 int main(int argc, char** args){
 	int listenFD=simpleOpenListenSocket(63036);
 	if(listenFD<0){
@@ -22,38 +55,17 @@ int main(int argc, char** args){
 		exit(1);
 	}
 
-	char buf;
 	while(1){
-		struct newConnectionInfo peer = simpleAccept(listenFD);
-		int newSockFD = peer.FD; //get the FD of the new client connection
-		if (newSockFD < 0) 
+		struct newConnectionInfo* peer=new struct newConnectionInfo;
+		*peer = simpleAccept(listenFD);
+
+		int newSockFD = peer->FD; //get the FD of the new client connection
+		if (newSockFD < 0)
 	          error("ERROR on accept");
-	    printf("\n\n=== New Connection ===\n%s\n\n\n",peer.address);
+	    printf("\n\n=== New Connection ===\n%s\n\n\n",peer->address);
 
-	    int charsRead=1;
-	    std::string header;
-	    while(charsRead){
-		    charsRead=read(newSockFD,&buf,1);//read the message
-		    if(charsRead<0) error("Unable to read socket");
-		    printf("%c", buf);
-		    header+=buf;
-
-		    int headerLength=header.length();
-		    if(headerLength<5) continue;
-		    if(header.substr(headerLength-4) == "\r\n\r\n"){
-		    	//found the end of the header of the newly connected client
-		    	//now lets break so that we can analyze what we need to do
-		    	break;
-		    }
-		}
-		printf("\n\n=== Writing ===\n\n");
-
-		if(header.substr(0,3)=="GET")
-			engageBrowser(newSockFD,"");
-		if(header.substr(0,4)=="POST")
-			engageBrowser(newSockFD,"");
-		if(header.substr(0,5)=="RASPI")
-			raspiConnection(peer,header);
+	    pthread_t threadFD;
+		pthread_create(&threadFD,NULL,spawnThread,peer);
 	}
 
 	printf("Exiting\n");
