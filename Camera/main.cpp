91128bab14bce6b3add9eaa9038629323948497f
glop102 <alexlikeschess@gmail.com>
2015-10-12 20:21:27 -0400

    Properly start of the program

--- Camera/main.cpp
+++ Camera/main.cpp
@@ -3,18 +3,24 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
+
+void firstConnect(){
+	int socketFD = simpleConnectToHost("192.168.1.1",63036);
+	while(socketFD<0){
+		sleep(5); //5 seconds
+		socketFD=simpleConnectToHost("192.168.1.1",63036);
+	}
+	char header[]="RASPI CAMERA\r\nREPORTING\r\n\r\n";
+	write(socketFD,header,strlen(header));
+	close(socketFD);
+}
 
 int main(int argc, char const *args[]){
-	int socketFD = simpleConnectToHost("127.0.1.2",63036);
-	if(socketFD<0){
-		printf("ERROR opening socket\n");
-		exit(1);
-	}
-	//char header[]="RASPI CAMERA\r\nREPORTING\r\n\r\n";
-	char header[]="RASPI CAMERA\r\nIMAGE SEND\r\n\r\n";
-	write(socketFD,header,strlen(header));
-	sendFileAlreadyConnected(socketFD,"127.0.0.1",63036,"../Examples/1430098684987.png");
-	close(socketFD);
+	system("");
+	firstConnect(); //connect to the admin to say that we are here
+
+	
 
 	return 0;
 }
