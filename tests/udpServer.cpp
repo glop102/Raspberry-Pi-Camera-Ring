98bffd1a4a3386a8f637dfbdd660377eb0e47455
glop102 <alexlikeschess@gmail.com>
2015-10-13 22:58:20 -0400

    test

--- tests/udpServer.cpp
+++ tests/udpServer.cpp
@@ -5,9 +5,9 @@
 int main(){
 	//int socketFD=simpleOpenSocket_UDPBroadcast(63036);
 	int socketFD=simpleOpenSocket_UDP(63036);
-	//send_UDP(socketFD,63036,"10.42.0.44","This Is It.");
-	send_UDP(socketFD,63036,"255.255.255.255","This Is It.");
-	//sendBroadcast_UDP(socketFD,63036,"This Is It.");
+	send_UDP(socketFD,63036,"10.42.0.44","Regulat");
+	send_UDP(socketFD,63036,"255.255.255.255","All Cast");
+	sendBroadcast_UDP(socketFD,63036,"Broadcast");
 	close(socketFD);
 	return 0;
 }
--- tests/udpServer_butTheOtherOne.cpp
+++ tests/udpServer_butTheOtherOne.cpp
@@ -5,8 +5,10 @@
 int main(){
 	//int socketFD=simpleOpenSocket_UDPBroadcast(63036);
 	int socketFD=simpleOpenSocket_UDP(63036);
-	struct newConnectionInfo peer = listen_UDP(socketFD);
-	printf("Got A Hit\n%s\n%s\n",peer.address,peer.message.c_str());
+	while(1){
+		struct newConnectionInfo peer = listen_UDP(socketFD);
+		printf("Got A Hit\n%s\n%s\n",peer.address,peer.message.c_str());
+	}
 	close(socketFD);
 	return 0;
 }
