135ecd34a5ad347f853623897ce645136f9a2566
glop102 <alexlikeschess@gmail.com>
2015-10-13 22:18:57 -0400

    Added udp tests

---
+++ tests/udpServer.cpp
@@ -0,0 +1,10 @@
+#include "../libs/stdSocketTools.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int socketFD=simpleOpenSocket_UDP(63036);
+	send_UDP(socketFD,63036,"10.42.0.44","This Is It.");
+	close(socketFD);
+	return 0;
+}
---
+++ tests/udpServer_butTheOtherOne.cpp
@@ -0,0 +1,11 @@
+#include "../libs/stdSocketTools.cpp"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int socketFD=simpleOpenSocket_UDP(63036);
+	struct newConnectionInfo peer = listen_UDP(socketFD);
+	printf("Got A Hit\n%s\n%s\n",peer.address,peer.message.c_str());
+	close(socketFD);
+	return 0;
+}
