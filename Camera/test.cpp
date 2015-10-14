660c2bc3e7b839a93f4a63486a0c5991cba5b6ba
glop102 <alexlikeschess@gmail.com>
2015-10-14 11:40:52 +0000

    imporved test program

--- Camera/test.cpp
+++ Camera/test.cpp
@@ -3,6 +3,7 @@
 #include "raspicam/src/raspicamtypes.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 void captureStill(raspicam::RaspiCam_Still& cam){
 	size_t bufSize = cam.getImageBufferSize();
@@ -18,18 +19,22 @@ void captureStill(raspicam::RaspiCam_Still& cam){
 }
 
 int main(){
-	raspicam::RaspiCam preview;
+	//raspicam::RaspiCam preview;
 	raspicam::RaspiCam_Still still;
-	if(!preview.open()){
-		printf("Unable to open Camera\n");
-		exit(1);
-	}
-	//if(!still.open()){
+	//preview.setFormat(raspicam::RASPICAM_FORMAT_RGB);
+	still.setEncoding(raspicam::RASPICAM_ENCODING_PNG);
+	still.setCaptureSize(1200,960);
+	//if(!preview.open()){
 	//	printf("Unable to open Camera\n");
 	//	exit(1);
 	//}
-	preview.setFormat(raspicam::RASPICAM_FORMAT_RGB);
-	still.setEncoding(raspicam::RASPICAM_ENCODING_PNG);
+	if(!still.open()){
+		printf("Unable to open Camera\n");
+		exit(1);
+	}
+	sleep(1); //wait for a stable image
+	//preview.setFormat(raspicam::RASPICAM_FORMAT_RGB);
+	//still.setEncoding(raspicam::RASPICAM_ENCODING_PNG);
 
 	captureStill(still);
 
