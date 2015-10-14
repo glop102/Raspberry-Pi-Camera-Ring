4e5338e1420c019b652ecc0ffc26ac5bfc24a0c2
glop102 <alexlikeschess@gmail.com>
2015-10-13 22:23:23 -0400

    test

--- tests/udpServer.cpp
+++ tests/udpServer.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(){
-	int socketFD=simpleOpenSocket_UDP(63036);
+	int socketFD=simpleOpenSocket_UDPBroadcast(63036);
 	send_UDP(socketFD,63036,"10.42.0.44","This Is It.");
 	close(socketFD);
 	return 0;
